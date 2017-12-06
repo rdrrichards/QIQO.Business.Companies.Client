c4ff8b36e0f4eccd33124e70982fd98b7a6581b5
R Richards <rdrrichards@hotmail.com>
2017-12-05 19:03:38 -0600

    address/address list coded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,24 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { PanelModule, InputTextModule, ButtonModule, DataTableModule } from 'primeng/primeng';
+import { PanelModule, InputTextModule, ButtonModule, DataTableModule, RadioButtonModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { AttributeComponent } from './attributes/attribute/attribute.component';
 import { AttributeListComponent } from './attributes/attribute-list/attribute-list.component';
 import { CompanyService } from './companies/company.service';
 import { HttpClientModule } from '@angular/common/http';
+import { AddressComponent } from './addresses/address/address.component';
+import { AddressListComponent } from './addresses/address-list/address-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AttributeComponent,
-    AttributeListComponent
+    AttributeListComponent,
+    AddressComponent,
+    AddressListComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +30,8 @@ import { HttpClientModule } from '@angular/common/http';
     PanelModule,
     InputTextModule,
     ButtonModule,
-    DataTableModule
+    DataTableModule,
+    RadioButtonModule
   ],
   providers: [ CompanyService ],
   bootstrap: [AppComponent]
