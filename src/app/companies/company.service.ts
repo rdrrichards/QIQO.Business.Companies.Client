11f68363cf32b7c8ac8e8a701a4ee63a15e90a35
R Richards <rdrrichards@hotmail.com>
2017-12-23 15:26:36 -0600

    error handling in service

--- src/app/companies/company.service.ts
+++ src/app/companies/company.service.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
+import { of } from 'rxjs/observable/of';
+import { catchError } from 'rxjs/operators';
 import { ApiResponse } from '../common/response';
 
 @Injectable()
@@ -12,12 +13,37 @@ export class CompanyService {
 
   getAllCompanies(): Observable<ApiResponse> {
     return this.httpClient
-      .get<ApiResponse>(`${environment.baseUrls.companies}`)
-      .map(response => response);
+      .get<ApiResponse>(`${environment.baseUrls.companies}`).pipe(
+        catchError(
+          this.handleGatewayError('getAllCompanies', new ApiResponse())
+        )
+      );
   }
   getCompany(id: string): Observable<ApiResponse> {
     return this.httpClient
-      .get<ApiResponse>(`${environment.baseUrls.companies}/${id}`)
-      .map(response => response);
+      .get<ApiResponse>(`${environment.baseUrls.companies}/${id}`).pipe(
+        catchError(
+          this.handleGatewayError('getCompany', new ApiResponse())
+        )
+      );
+  }
+
+  private handleGatewayError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log('An error occured!', operation);
+      console.log('error', error);
+      switch (error.status) {
+        case 0:
+          console.log(error.statusText);
+          break;
+        case 404:
+          console.log('404');
+          break;
+        default:
+          console.log('default');
+          break;
+      }
+      return of(result as T);
+    };
   }
 }
