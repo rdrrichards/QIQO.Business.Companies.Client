01679b6154e8174b149ca88711972d78b3b20053
R. Richards <rdrrichards@hotmail.com>
2020-02-26 17:09:51 -0600

    entity address to ng 9.0.0

--- projects/entity-address/src/lib/address-list/address-list.component.ts
+++ projects/entity-address/src/lib/address-list/address-list.component.ts
@@ -4,12 +4,14 @@ import { Address } from '../address';
 @Component({
   selector: 'app-address-list',
   template: `<p-table [value]="addresses" class="ui-g-12">
-    <p-column field="addressId" header="Address Id" hidden="hidden"></p-column>
-    <p-column field="addressType.addressTypeName" header="Address Type"></p-column>
-    <p-column field="addressLine1" header="Address Line 1"></p-column>
-    <p-column field="addressCity" header="City"></p-column>
-    <p-column field="addressState" header="State"></p-column>
-    <p-column field="addressPostalCode" header="Postal"></p-column>
+    <tr>
+      <td field="addressId" header="Address Id" hidden="hidden"></td>
+      <td field="addressType.addressTypeName" header="Address Type"></td>
+      <td field="addressLine1" header="Address Line 1"></td>
+      <td field="addressCity" header="City"></td>
+      <td field="addressState" header="State"></td>
+      <td field="addressPostalCode" header="Postal"></td>
+    </tr>
   </p-table>`
 })
 export class AddressListComponent {
