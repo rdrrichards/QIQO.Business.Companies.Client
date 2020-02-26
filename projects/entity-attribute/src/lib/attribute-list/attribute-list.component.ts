364da04f1e05a6bf236d6ca328c12895ebd8c464
R. Richards <rdrrichards@hotmail.com>
2020-02-26 17:09:34 -0600

    entity attribute to ng 9.0.0

--- projects/entity-attribute/src/lib/attribute-list/attribute-list.component.ts
+++ projects/entity-attribute/src/lib/attribute-list/attribute-list.component.ts
@@ -4,12 +4,14 @@ import { Attribute } from '../attribute';
 @Component({
   selector: 'app-attribute-list',
   template: `<p-table [value]="attributes" class="ui-g-12">
-    <p-column field="attributeId" header="Attribute Id" hidden="hidden"></p-column>
-    <p-column field="attributeType.attributeTypeName" header="Attribute Type Name"></p-column>
-    <p-column field="attributeValue" header="Attribute Value"></p-column>
-    <p-column field="attributeDataType" header="Data Type"></p-column>
-    <p-column field="attributeDisplayFormat" header="Display Format"></p-column>
-    <p-column field="attributeType.attributeTypeDesc" header="Description"></p-column>
+    <tr>
+    <td field="attributeId" header="Attribute Id" hidden="hidden"></td>
+    <td field="attributeType.attributeTypeName" header="Attribute Type Name"></td>
+    <td field="attributeValue" header="Attribute Value"></td>
+    <td field="attributeDataType" header="Data Type"></td>
+    <td field="attributeDisplayFormat" header="Display Format"></td>
+    <td field="attributeType.attributeTypeDesc" header="Description"></td>
+    </tr>
   </p-table>`
 })
 export class AttributeListComponent {
